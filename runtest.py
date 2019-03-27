590b3ae3893209ce4fa234d77c5089107fbc498a
Mollylin0 <365837434@qq.com>
2019-03-27 15:07:18 +0800

    first commit

--- runtest.py
+++ runtest.py
@@ -5,6 +5,7 @@ from BeautifulReport import BeautifulReport
 test_dir = './testcase'
 discover = unittest.defaultTestLoader.discover(test_dir, pattern='*testcase.py')
 
+
 if  __name__ == '__main__':
     # 从这里执行
     now = time.strftime("%Y-%m-%d %H_%M_%S")
